736dfa333b47e80b06f796345de4009a11bc328e
alexbournemsk <arbiter1@yandex.ru>
2021-12-08 16:48:21 +0300

    added "view" - modules for MVP

--- src/main.js
+++ src/main.js
@@ -1,14 +1,38 @@
 import {createNavigationTemplate} from './view/navigation.js';
+import { createFilterTemplate } from './view/filters.js';
+import { createSortTemplate } from './view/sort.js';
+import { createTripInfoTemplate } from './view/info.js';
+import { createTripCostTemplate } from './view/cost.js';
+import { createNewPointTemplate } from './view/new-point.js';
+import { createEditPointTemplate } from './view/edit-point.js';
+import { pointTemplate } from './view/point.js';
 
-//alert('hi!')
-const render = (container,template,place) => {
+const COUNT = 3;
+const DEFAULT_INSERT_POSITION = 'beforeend';
+//alert('hi!');
+const render = (container,template,place=DEFAULT_INSERT_POSITION) => {
   container.insertAdjacentHTML(place,template);
 };
 
-const tripMainElement = document.querySelector('trip-main');
-const tripNavigationElement = document.querySelector('trip-controls__navigation');
-console.log(tripMainElement);
-console.log(tripNavigationElement);
 
-render(tripNavigationElement,createNavigationTemplate,'beforeend');
+const tripMainElement = document.querySelector('.trip-main');
+render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
+
+const tripNavigationElement = document.querySelector('.trip-controls__navigation');
+render(tripNavigationElement,createNavigationTemplate());
+
+const tripInfoContainer = tripMainElement.querySelector('.trip-info');
+render(tripInfoContainer, createTripCostTemplate());
+
+const tripFiltersElement = document.querySelector('.trip-controls__filters');
+render(tripFiltersElement,createFilterTemplate());
+
+const tripEventsElement = document.querySelector('.trip-events');
+render(tripEventsElement, createSortTemplate());
+render(tripEventsElement, createEditPointTemplate());
+render(tripEventsElement, createNewPointTemplate());
+
+for (let i = 0; i < COUNT; i++) {
+  render(tripEventsElement, pointTemplate());
+}
 
