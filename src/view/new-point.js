ebb70acb5cabee6b1c6a90406574528100cd04d0
alexbournemsk <arbiter1@yandex.ru>
2022-01-20 19:37:33 +0300

    WIP

--- src/view/new-point.js
+++ src/view/new-point.js
@@ -1,4 +1,4 @@
-export const createNewPointTemplate = () => (`
+const createNewPointTemplate = () => (`
 <li class="trip-events__item">
 <form class="event event--edit" action="#" method="post">
   <header class="event__header">
@@ -164,3 +164,4 @@ export const createNewPointTemplate = () => (`
 
 `);
 
+//export class NewPoint
